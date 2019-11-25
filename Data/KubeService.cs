32a3dd7ff9a0d0cd332d8d172a29e9f4e1cbc408
frivard <frivard@coveo.com>
2019-11-25 16:07:34 -0500

    Initial commit

---
+++ Data/KubeService.cs
@@ -0,0 +1,27 @@
+﻿using k8s;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kubics.Data
+{
+    public class KubeService
+    {
+        public KubeService()
+        {
+        }
+
+        public async Task<IList<string>> ListClustersAsync()
+        {
+            List<string> clusters = new List<string>();
+            var conf = await KubernetesClientConfiguration.LoadKubeConfigAsync();
+            foreach (var cluster in conf.Clusters)
+            {
+                clusters.Add(cluster.Name);
+            }
+
+            return clusters;
+        }
+    }
+}
---
+++ Pages/Clusters.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kubics.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace Kubics.Pages
+{
+    public partial class Clusters : ComponentBase
+    {
+        public string[] Names { get; set; }
+
+        [Inject] KubeService K8sService { get; set; }
+
+
+        public Clusters()
+        {
+            Names = new string[0];
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Names = (await K8sService.ListClustersAsync()).ToArray();
+        }
+    }
+}
