288117cca3dd74fa45cb6eeffbc0fe9f59e2acc2
frivard <frivard@coveo.com>
2019-11-25 16:49:26 -0500

    Add a dropdown component for contexts

---
+++ Pages/Contexts.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using k8s;
+
+namespace Kubics.Pages
+{
+    public partial class Contexts : ComponentBase
+    {
+        public string[] Names { get; set; }
+
+        public string Current { get; set; }
+
+        public Contexts()
+        {
+            Names = new string[1];
+            Names[0] = "<loading>";
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var conf = await KubernetesClientConfiguration.LoadKubeConfigAsync();
+            List<string> names = new List<string>();
+            foreach (var context in conf.Contexts)
+            {
+                names.Add(context.Name);
+            }
+            Current = conf.CurrentContext;
+            Names = names.ToArray();
+        }
+
+    }
+}
